3980879d7f711004233a8ca23fef80c003af0af1
AhmadovKomronbek <komronhtmlcss@gmail.com>
2024-06-18 17:04:49 +0500

    done

---
+++ main.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+import pandas as pd
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+# starter data
+data_csv = pd.read_csv("data.csv")
+data_dict = data_csv.to_dict(orient="records")
+now_time = datetime.now()
+with open("data_struckture.txt") as file:
+    struck = file.read()
+
+# functions
+def check_data_time(user_time, time_now):
+    user_day = user_time["day"]
+    user_month = user_time["month"]
+    time_day = int(time_now.strftime("%d"))
+    time_month = int(time_now.strftime("%m"))
+    if user_day == time_day and user_month == time_month:
+        return True
+
+def send_email(email, name):
+    global struck
+
+    email_sender = "komronhtmlcss@gmail.com"
+    email_password = "mcxs chux adtg kwwh"
+
+    email_receiver = email
+
+    subject = f"Salom {name}"
+    body = struck.replace("[name]", f"{name}")
+
+    em = EmailMessage()
+    em["From"] = email_sender
+    em["To"] = email_receiver
+    em["subject"] = subject
+    em.set_content(body)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
+        print("sented")
+
+# main code
+for row in data_dict:
+    if check_data_time(row, now_time):
+        send_email(row["email"], row["name"])
